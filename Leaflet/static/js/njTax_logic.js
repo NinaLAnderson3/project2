c8b36452cdf02a2ffa0a8773ff06d77fcf1cd0e7
DanZanoria <71779837+DanZanoria@users.noreply.github.com>
2021-03-16 18:24:14 -0400

    commit before merge

--- Leaflet/static/js/njTax_logic.js
+++ Leaflet/static/js/njTax_logic.js
@@ -55,17 +55,7 @@ info.onAdd = function(){
 // //add the info legend to the map
 info.addTo(myMap);
 
-// Create a Darth Vader Icon
-var DarthVader = L.icon({
-  iconUrl: 'icon/Darth-Vader-icon.png',
- 
 
-  iconSize:     [38, 95], // size of the icon
-  shadowSize:   [50, 64], // size of the shadow
-  iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-  shadowAnchor: [4, 62],  // the same for the shadow
-  popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-});
 
 // Use this link to get the geojson data.
 // var link = "static/data/merge__nj_geojson.geojson";
@@ -73,19 +63,69 @@ var link = "http://data.ci.newark.nj.us/dataset/db87f66a-6d79-4933-9011-f392fdce
 
 var markerData = "static/data/final_data.geojson";
 
+
+
 d3.json(markerData, function(mData){
   // console.log(mData.features.length)
   for (var i=0; i<mData.features.length; i++) {
+    
+    
+  
+  // Create a Darth Vader Icon
+  var IconSize = mData.features[i].properties.SummativeScore
+  
+// Create an Icon Class TheForce
+var TheForceIcon = L.Icon.extend({
+  options: {
+        iconSize:     IconSize,
+        iconAnchor:   [40, -20],
+        popupAnchor:  [-3, -76]
+    }
+
+})
+
+
+// Add the Different Icons to TheForce Class
+  var VaderIcon = new TheForceIcon({iconUrl: 'icon/Darth-Vader-icon.png'}),
+      R2D2icon = new TheForceIcon({iconUrl: 'icon/R2D2-icon.png'}),
+      AhsokaIcon = new TheForceIcon({iconUrl: 'icon/Ahsoka-Tano-icon.png'}),
+      LukeIcon = new TheForceIcon({iconUrl: 'icon/Luke-Skywalker-01-icon.png'}),
+      LeiaIcon = new TheForceIcon({iconUrl: 'icon/Leia-icon.png'}),
+      YodaIcon = new TheForceIcon({iconUrl: 'icon/Master-Joda-icon.png'})
+
+function SelectIcon(StarWars) {
+  switch(true) {
+      case (1.0 <= StarWars && StarWars <= 1.5):
+          return YodaIcon;
+      case (1.5 <= StarWars && StarWars<= 2.0):
+        return LeiaIcon;
+      case (2.0 <= StarWars && StarWars<= 2.5):
+          return AhsokaIcon;
+      case (3.0 <= StarWars && StarWars<= 3.5):
+          return LukeIcon;
+      case (3.5 <= StarWars && StarWars <= 4.0):
+        return VaderIcon;
+      default:
+        return R2D2icon;
+  }
+}
+
+// var DarthVader = L.icon({
+//   iconUrl: 'icon/Darth-Vader-icon.png',
+//   iconSize:     IconSize * 10, // size of the icon
+//   iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+//   popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+// });
     var location = mData.features[i].geometry.coordinates;
     console.log(location[1], location [0]);
     // if (location){
-      L.marker([location[1], location[0]],{icon: DarthVader})
-      .bindPopup("<h2>"+mData.features[i].properties.County)
+      L.marker([location[1], location[0]],{icon: SelectIcon (mData.features[i].properties["2020_tax_rate"])})
+      .bindPopup("<h2>"+mData.features[i].properties.County + "</h2> <h3> Summative Score " + mData.features[i].properties.SummativeScore + "</h3> <h4> Tax Rate : " + mData.features[i].properties["2020_tax_rate"] + "</h>")
       .addTo(myMap);
 
     //   console.log(mData.features[i].geometry.coordinates[1], mData.features[i].geometry.coordinates[0])
     
-
+  
   }
 
 
@@ -124,16 +164,12 @@ d3.json(link, function(data) {
       return{
         color: "gray",
         fillColor: "green",
-        fillOpacity:countyDepth(features.properties.tax_rate)
+        fillOpacity:countyDepth(features.properties.tax_rate) * 0.75
       }
   }
   }).addTo(myMap);
 })
 
-// L.marker([45.52, -122.67]).addTo(myMap);
-L.marker([39.4431,-74.61701363636367])
-.bindPopup("Hi")
-.addTo(myMap)
 
 function createFeatures(data){
   // console.log(data)
